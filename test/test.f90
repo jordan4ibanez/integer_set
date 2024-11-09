9303d1d41d78e73409f2072bbc4645a552482a74
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-09 04:37:36 -0500

    forgot to push the dev test

--- test/test.f90
+++ test/test.f90
@@ -1,4 +1,20 @@
 program test
-  
+  use, intrinsic :: iso_c_binding
+  use :: integer32_set
+  implicit none
+
+  type(int32_set) :: data
+  integer(c_int32_t) :: i, j
+
+
+  data = new_int32_set()
+
+  do i = 1,10
+    j = 1
+    call data%push(j)
+  end do
+
+  print*,data%size()
+
 
 end program test
