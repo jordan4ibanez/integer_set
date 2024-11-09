4ff25bc33c6ed4f7919d72a282bd0e655404dd62
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-09 04:28:12 -0500

    format

--- src/integer32_set.f90
+++ src/integer32_set.f90
@@ -1,42 +1,49 @@
 module integer32_set
-   use, intrinsic :: iso_c_binding
-   implicit none
+  use, intrinsic :: iso_c_binding
+  implicit none
 
 
-   type :: int32_set
-      integer(c_int32_t), dimension(:), pointer :: data => null()
-      integer(c_int32_t) :: size = 0
-   contains
+  type :: int32_set
+    integer(c_int32_t), dimension(:), pointer :: data => null()
+    integer(c_int32_t) :: size = 0
+  contains
 
-   end type int32_set
+  end type int32_set
 
 
 contains
 
 
-   function new_int32_set() result(a)
-      implicit none
+  function new_int32_set() result(a)
+    implicit none
 
-      type(int32_set) :: a
+    type(int32_set) :: a
 
-      allocate(a%data(0))
-   end function new_int32_set
+    allocate(a%data(0))
+  end function new_int32_set
 
 
-   subroutine int32_set_push(this, data)
-      implicit none
+  subroutine int32_set_push(this, data)
+    implicit none
 
-      class(int32_set), intent(inout) :: this
-      integer(c_int32_t), intent(in), value :: data
-      integer(c_int32_t) :: i
-      logical(c_bool) :: found
+    class(int32_set), intent(inout) :: this
+    integer(c_int32_t), intent(in), value :: data
+    integer(c_int32_t) :: i
+    logical(c_bool) :: found
 
-      found = .false.
+    found = .false.
 
-      do i = 1,this%size
+    do i = 1,this%size
+      if (this%data(i) == data) then
+        found = .true.
+        exit
+      end if
+    end do
 
-      end do
-   end subroutine int32_set_push
+    if (.not. found) then
+
+    end if
+  end subroutine int32_set_push
 
 
 end module integer32_set
